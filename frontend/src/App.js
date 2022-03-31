b4065395a5debbfad6801516e661a12cd819343e
clam <lam.clarence.bc@gmail.com>
2022-03-30 23:53:41 -0700

    complete app create edit delete functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,42 @@
 import React, { Component } from "react";
 import Modal from "./components/Modal";
+import axios from "axios";
 
-const todoItems = [
-  {
-    id: 1,
-    title: "Go to Market",
-    description: "Buy ingredients to prepare dinner",
-    completed: true,
-  },
-  {
-    id: 2,
-    title: "Study",
-    description: "Do history readings and history assignment",
-    completed: false,
-  },
-  {
-    id: 3,
-    title: "Sammy's books",
-    description: "Go to library to return Sammy's books",
-    completed: true,
-  },
-  {
-    id: 4,
-    title: "Article",
-    description: "Write article on how to use Django with React",
-    completed: false,
-  },
-];
+// Sample todo items
+
+// const todoItems = [
+//   {
+//     id: 1,
+//     title: "Go to Market",
+//     description: "Buy ingredients to prepare dinner",
+//     completed: true,
+//   },
+//   {
+//     id: 2,
+//     title: "Study",
+//     description: "Do history readings and history assignment",
+//     completed: false,
+//   },
+//   {
+//     id: 3,
+//     title: "Sammy's books",
+//     description: "Go to library to return Sammy's books",
+//     completed: true,
+//   },
+//   {
+//     id: 4,
+//     title: "Article",
+//     description: "Write article on how to use Django with React",
+//     completed: false,
+//   },
+// ];
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       viewCompleted: false,
-      todoList: todoItems,
+      todoList: [],
 
       modal: false,
       activeItem: {
@@ -44,20 +47,43 @@ class App extends Component {
     };
   }
 
+  componentDidMount() {
+    this.refreshList();
+  }
+
+  // Function to update Todo list to the most recent list. Use after API request is complete
+  refreshList = () => {
+    axios
+    .get("api/todos/")
+    .then((res) => this.setState({ todoList: res.data }))
+    .catch((err) => console.log(err));
+  };
+
   // Functions to enable modal functionality
 
   toggle = () => {
     this.setState({ modal: !this.state.modal });
   };
 
+  // handleSubmit Function to take care of Create and Update operations
   handleSubmit = (item) => {
     this.toggle();
 
-    alert("save" + JSON.stringify(item));
+    if (item.id) {
+      axios
+        .put(`/api/todos/${item.id}/`, item)
+        .then((res) => this.refreshList());
+      return;
+    }
+    axios
+      .post("/api/todos/", item)
+      .then((res) => this.refreshList());
   };
 
   handleDelete = (item) => {
-    alert("delete" + JSON.stringify(item));
+    axios 
+      .delete(`/api/todos/${item.id}`)
+      .then((res) => this.refreshList());
   };
 
   createItem = () => {
@@ -101,7 +127,7 @@ class App extends Component {
   renderItems = () => {
     const { viewCompleted } = this.state;
     const newItems = this.state.todoList.filter(
-      (item) => item.completed == viewCompleted
+      (item) => item.completed === viewCompleted
     );
 
     return newItems.map((item) => (
